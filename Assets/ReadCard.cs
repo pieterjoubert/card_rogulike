c32ba3e71eb836d9acc788da7ee718b048b9f578
Pieter Joubert <Joubert>
2020-10-13 19:50:47 +0200

    Initial Commit

---
+++ Assets/ReadCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadCard : MonoBehaviour
+{
+    bool up = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        if(!up)
+        {
+            this.transform.position += transform.up * 0.5f;
+            up = true;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if(up)
+        {
+            this.transform.position += transform.up * -0.5f;
+            up = false;
+        }
+    }
+}
